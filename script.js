c0e5cb52280b493e88c59d3f11b56e9d285278f6
Skyll007 <abdo.ashour2005@gmail.com>
2026-05-30 20:40:04 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log("JS WORKING");
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  let signInBtn = document.getElementById("signin-btn");
+  let modal = document.getElementById("signin-modal");
+
+  console.log("BTN =", signInBtn); // 👈 مهم جداً
+
+  if (!signInBtn || !modal) {
+    console.log("ELEMENTS NOT FOUND");
+    return;
+  }
+
+  signInBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("clicked");
+    modal.classList.add("show");
+  });
+
+  window.addEventListener("click", function(e){
+    if (e.target === modal) {
+      modal.classList.remove("show");
+    }
+  });
+
+});
+function closeModal(){
+  document.getElementById("signin-modal").classList.remove("show");
+}
+
+function saveUser(){
+  let username = document.getElementById("username").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let error = document.getElementById("error");
+  let btn = document.getElementById("signin-btn");
+
+  // validation 
+  if(username === "" || email === "" || !email.includes("@")){
+    error.style.display = "block";
+    return;
+  }
+
+  error.style.display = "none";
+
+  localStorage.setItem("username", username);
+  localStorage.setItem("email", email);
+
+  btn.innerText = username;
+
+  closeModal();
+}
